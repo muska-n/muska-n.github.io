a094a2781a7050bd1045ee1cb4c00c99567e3663
muska-n <153828962+muska-n@users.noreply.github.com>
2023-12-15 11:33:42 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,64 @@ document.querySelector("#form").addEventListener("submit", event =>{
   console.log(event.target.col.value)
   document.body.style.background = event.target.col.value
 })
-//make it so dark background = light text and vice versa
-//add columns with bootstrap
-//due 15/12/23
-//add another question maybe-give it tabs at the top instead of like this
+
+const tabElements = document.querySelectorAll('button[role="tab"]');
+const panelElements = document.querySelectorAll('[role="tabpanel"]');
+let activeIndex = 0;
+
+tabElements.forEach(function (tab, index) {
+  tab.addEventListener("click", function (event) {
+    setActiveTab(index);
+  });
+});
+
+function setActiveTab(index) {
+  tabElements[activeIndex].setAttribute("aria-selected", "false");
+  tabElements[activeIndex].tabIndex = -1;
+
+  tabElements[index].setAttribute("aria-selected", "true");
+  tabElements[index].tabIndex = 0;
+  tabElements[index].focus();
+
+  setActivePanel(index);
+  activeIndex = index;
+}
+function setActivePanel(index) {
+  panelElements[activeIndex].setAttribute("hidden", "");
+  panelElements[activeIndex].tabIndex = -1;
+  panelElements[index].removeAttribute("hidden");
+  panelElements[index].tabIndex = 0;
+}
+
+tabElements.forEach(function (tab, index) {
+  
+  tab.addEventListener("keydown", function (event) {
+    const lastIndex = tabElements.length - 1;
+
+    if (event.code === "ArrowLeft" || event.code === "ArrowUp") {
+      event.preventDefault();
+
+      if (activeIndex === 0) {
+        setActiveTab(lastIndex);
+      } else {
+        setActiveTab(activeIndex - 1);
+      }
+    } else if (event.code === "ArrowRight" || event.code === "ArrowDown") {
+      event.preventDefault();
+
+      if (activeIndex === lastIndex) {
+        setActiveTab(0);
+      } else {
+        setActiveTab(activeIndex + 1);
+      }
+    } else if (event.code === "Home") {
+      event.preventDefault();
+
+      setActiveTab(0);
+    } else if (event.code === "End") {
+      event.preventDefault();
+
+      setActiveTab(lastIndex);
+    }
+  });
+});
